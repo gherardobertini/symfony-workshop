3004beac85fd3af3de7499c210e62f28bd6e2451
Simone D'Amico <simone.damico@spreaker.com>
2020-11-15 09:55:52 +0100

    Created home page skeleton

---
+++ app/src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    public function homepage(): Response
+    {
+        return $this->render('home/homepage.html.twig');
+    }
+}
