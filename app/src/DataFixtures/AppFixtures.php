503db4470fd2d5f965c17b445a8a231ace262b18
Simone D'Amico <dymissy86@gmail.com>
2021-02-16 20:14:38 +0100

    Added user security fields

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -21,15 +21,21 @@ class AppFixtures extends Fixture
     private function createRyanContents(ObjectManager $manager): void
     {
         $ryan = new User();
+        $ryan->setEmail('ryan@ryan.it');
+        $ryan->setPassword('ryan');
         $ryan->setUsername('ryan');
         $ryan->setFullName('Ryan Scheinder');
         $ryan->setAvatar('/assets/img/ryan.jpg');
+        $ryan->setCreatedAt(new DateTimeImmutable());
 
         $post = new Post();
         $post->setTitle('Enjoy the story you are living');
         $post->setContent('Sometimes you have to let go of the picture of what you thought it would be like and learn to find joy in the story you are actually living.');
         $post->setAuthor($ryan);
         $post->setCreatedAt(new DateTimeImmutable('2020-05-02 11:00:00'));
+
+        $manager->getRepository(User::class)
+            ->upgradePassword($ryan, 'ryan');
 
         $manager->persist($ryan);
         $manager->persist($post);
@@ -38,9 +44,12 @@ class AppFixtures extends Fixture
     private function createJulieContents(ObjectManager $manager): void
     {
         $julie = new User();
+        $julie->setEmail('julie@julie.it');
+        $julie->setPassword('julie');
         $julie->setUsername('julie');
         $julie->setFullName('Julie Standford');
         $julie->setAvatar('/assets/img/julie.jpg');
+        $julie->setCreatedAt(new DateTimeImmutable());
 
         $post = new Post();
         $post->setTitle('Live for what today has to offer');
